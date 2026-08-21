a6838dc1d308a71780da0983995914a05724bf4f
Stevenhalim5224 <stevenhalim5224@gmail.com>
2026-08-21 21:10:14 +0700

    test

---
+++ blog/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        HALAMAN UTAMA SUDAH JALAN! CSS BERHASIL!
+      </h1>
+    </div>
+  );
+}
